1dbc83c5205df247e14c466938951fec43c1d63e
Eduardo Knoop <eduardoo.knoop@gmail.com>
2025-06-02 14:56:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-function calcularIMC(peso, altura) {
-    let imc = peso / (altura * altura);
-    return imc.toFixed(2); // arredonda pra 2 casas decimais
-}
-// Exemplo:
-console.log(calcularIMC(70, 1.75)); // Resultado: 22.86
+
